88ac485372fdfec6cdf6fca2fb6cd9d1a2092cb4
Semyon Gladyshev <gladyshev15081508@gmail.com>
2024-11-14 23:53:25 +0500

    first page + start of the form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Form.css"
+
+function Form() {
+    return (
+        <div className="form-block">
+            <h3>Оставьте заявку</h3>
+            <form>
+                <div className="f-input">
+                    <label for="femail">Электронная почта</label>
+                    <input type="email" id="femail" name="femail" placeholder="example@mail.ru"/>
+                </div>
+
+                <div className="f-input">
+                    <label for="fphone">Ваш телефон</label>
+                    <input type="tel" id="fphone" name="fphone" placeholder="+7 (900) 123 99-99"/>
+                </div>
+                
+                <div className="f-input">
+                    <label for="fname">Ваше имя</label>
+                    <input type="text" id="fname" name="fname" placeholder="Иванов Иван Иванович"/>
+                </div>
+                
+                <div className="f-input">
+                    <label for="ftype">Тип абонемента</label>
+                    <select id="ftype" name="ftype">
+                        <option value="all-inclusive">Все включено</option>
+                        <option value="student">Студенческий</option>
+                        <option value="day">Дневной</option>
+                    </select>
+                </div>
+                <button type="submit">Оставить заявку</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,56 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import backgroundImg1 from "../../images/home_bc_1.png"
+import backgroundImg2 from "../../images/home_bc_2.png"
+import "./HomePage.css"
+import arrowImg from "../../images/arrow.svg"
+import bicepsImg from "../../images/biceps.svg"
+import saunaImg from "../../images/sauna.svg"
+import boxImg from "../../images/box.svg"
+import Form from "../Form/Form";
 
 function HomePage() {
     return (
         <div>
             <Header/>
-            <p>HomePage</p>
+            <div className="first-block">
+                <img src={backgroundImg1} className="first-image"/>
+                <div className="content-overlay">
+                    <h2>Рассчитайте свою норму калорий прямо сейчас!</h2>
+                    <a href='/count_calories'>Рассчитать <img src={arrowImg}/></a>
+                </div>
+            </div>
+            <div className="divider-background"/>
+            <div className="second-block">
+                <img src={backgroundImg2} className="second-image"/>
+                <div className="content-overlay">
+                    <h2>Читайте новости нашего клуба!</h2>
+                    <a href='/news'>Читать <img src={arrowImg}/></a>
+                </div>
+            </div>
+            <div className="features">
+                <h2>Особенности нашего клуба:</h2>
+                <div className="features-list">
+                    <div className="feature">
+                        <img src={bicepsImg}/>
+                        <h3>Тренерский состав</h3>
+                    </div>
+                    <div className="feature">
+                        <img src={saunaImg} />
+                        <h3>Банный комплекс</h3>
+                    </div>
+                    <div className="feature">
+                        <img src={boxImg} />
+                        <h3>Групповые тренировки</h3>
+                    </div>
+                </div>
+                <a href="/about">Подробнее в разделе "О нас"<img src={arrowImg} className="btn-img"/></a>
+            </div>
+            <div className="form">
+                <h2>Закажите абонемент прямо сейчас!</h2>
+            </div>
+            <Form/>
             <Footer/>
         </div>
     );

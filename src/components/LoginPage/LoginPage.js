e1db3640ca10be3f82bbf1779556ba5d32437d92
sdobrimutrom <gladyshev15081508@gmail.com>
2024-11-15 16:12:25 +0500

    login form started

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "../Header/Header";
+import './Login.css'
 
 function LoginPage() {
     return (
         <div>
             <Header/>
-            <p>Login Page</p>
+            <div className="login-form">
+                <h3>Вход в аккаунт</h3>
+                <form>
+                    <label for="femail">Электронная почта</label>
+                    <input type="email" id="femail" name="femail" placeholder="example@mail.com"/>
+
+                    <label for="fpassword">Пароль</label>
+                    <input type="password" id="fpass" name="fpass" placeholder="Ваш пароль..."/>
+
+                    <a href="/" className="login-btn">Войти</a>
+                    <a href="/signin" className="sigin-btn">Регистрация</a>
+                </form>
+            </div>
         </div>
     );
 }

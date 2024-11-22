6cdde985285051f6b1a0763dfaecd8531eadc1e3
sdobrimutrom <gladyshev15081508@gmail.com>
2024-11-22 17:49:18 +0500

    count calories finished

--- src/components/CountCaloriesPage/CalorieCounter.jsx
+++ src/components/CountCaloriesPage/CalorieCounter.jsx
@@ -1,4 +1,9 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import "./caloriesCounter.css";
+import ProductSlider from "./ProductSlider";
+import cottageCheeseImg from "../../images/cottageCheese.png";
+import eggsImg from "../../images/eggs.png";
+import chickenImg from "../../images/chicken.png";
 
 const CalorieCounter = () => {
     const [gender, setGender] = useState(null);
@@ -28,47 +33,99 @@ const CalorieCounter = () => {
         setCalories(Math.round(bmr));
     }
 
+    const products = [
+        {
+            id: 1,
+            name: "Творог",
+            image: cottageCheeseImg,
+            kbzu: { calories: 145, protein: 21, fat: 5, carbs: 3},
+        },
+        {
+            id: 2,
+            name: "Куриные яйца",
+            image: eggsImg,
+            kbzu: { calories: 157, protein: 12.7, fat: 11.5, carbs: 0.7},
+        },
+        {
+            id: 3,
+            name: "Куриная грудка",
+            image: chickenImg,
+            kbzu: { calories: 113, protein: 23.6, fat: 1.9, carbs: 0.4},
+        },
+        {
+            id: 4,
+            name: "Творог",
+            image: cottageCheeseImg,
+            kbzu: { calories: 145, protein: 21, fat: 5, carbs: 3},
+        },
+        {
+            id: 5,
+            name: "Куриные яйца",
+            image: eggsImg,
+            kbzu: { calories: 157, protein: 12.7, fat: 11.5, carbs: 0.7},
+        },
+        {
+            id: 6,
+            name: "Куриная грудка",
+            image: chickenImg,
+            kbzu: { calories: 113, protein: 23.6, fat: 1.9, carbs: 0.4},
+        },
+    ];
+
     return (
-        <div>
-            <div>
-                <h3>Ваш пол:</h3>
-                <button onClick={() => setGender("male")}>Мужской</button>
-                <button onCanPlay={() => setGender("female")}>Женский</button>
-            </div>
-            <div>
-                <h3>Ваш вес:</h3>
-                <input type="number"
-                    placeholder="Вес (кг)"
-                    value={weight}
-                    onChange={(e) => setWeight(e.target.value)}/>
-                
-                <h3>Ваш рост:</h3>
-                <input type="number"
-                    placeholder="Рост (см)"
-                    value={height}
-                    onChange={(e) => setHeight(e.target.value)}/>
-
-                <h3>Ваш возраст:</h3>
-                <input type="number"
-                    placeholder="Возраст (лет)"
-                    value={age}
-                    onChange={(e) => setAge(e.target.value)}/>
-            </div>
-            <div>
-                <h3>Ваша цель:</h3>
-                <button onClick={() => setGoal("gain")}>Набрать массу</button>
-                <button onClick={() => setGoal("lose")}>Похудеть</button>
-                <button onClick={() => setGoal("maintain")}>Остаться в форме</button>
-            </div>
-
-            <button onClick={calculateCalories}>Рассчитать</button>
-
-            {calories && (
-                <div>
-                    <h3>Ваша норма калорий: {calories} ккал</h3>
+        <>
+            <div className="calories-counter">
+                <div className="set-sex">
+                    <h3>Ваш пол:</h3>
+                    <button onClick={() => setGender("male")}
+                        className={`button ${gender === "male" ? "selected" : ""}`}>Мужской</button>
+                    <button onClick={() => setGender("female")}
+                        className={`button ${gender === "female" ? "selected" : ""}`}>Женский</button>
                 </div>
-            )}
-        </div>
+                <div>
+                    <div className="set-specs">
+                        <h3>Ваш вес:</h3>
+                        <input type="number"
+                            placeholder="Вес (кг)"
+                            value={weight}
+                            onChange={(e) => setWeight(e.target.value)}/>
+                        <h3>Ваш рост:</h3>
+                        <input type="number"
+                            placeholder="Рост (см)"
+                            value={height}
+                            onChange={(e) => setHeight(e.target.value)}/>
+                    </div>
+                    <div className="set-age">
+                        <h3>Ваш возраст:</h3>
+                        <input type="number"
+                            placeholder="Возраст (лет)"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="set-goal">
+                    <h3>Ваша цель:</h3>
+                    <button onClick={() => setGoal("gain")}
+                        className={`button ${goal === "gain" ? "selected" : ""}`}>Набрать массу</button>
+                    <button onClick={() => setGoal("lose")}
+                        className={`button ${goal === "lose" ? "selected" : ""}`}>Похудеть</button>
+                    <button onClick={() => setGoal("maintain")}
+                        className={`button ${goal === "maintain" ? "selected" : ""}`}>Остаться в форме</button>
+                </div>
+                <button onClick={calculateCalories} className="count-btn">Рассчитать</button>
+            </div>
+            <div>
+                {calories && (
+                    <div>
+                        <div className="total-calories">
+                            <h3>Ваша норма калорий: <span>{calories} ККАЛ</span></h3>
+                        </div>
+                        <ProductSlider products={products}/>
+                    </div>
+                )}
+            </div>
+        </>
+        
         
     );
 };
--- src/components/CountCaloriesPage/ProductSlider.jsx
+++ src/components/CountCaloriesPage/ProductSlider.jsx
@@ -1,31 +1,42 @@
 import React from "react";
 import Slider from "react-slick";
+import "./productSlider.css"
+
 
 
 const ProductSlider = ({products}) => {
     const settings = {
         dots: true,
+        centerMode: true,
+        centerPadding: "50px",
         infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
     };
 
+
     return (
-        <Slider {...settings}>
-            {products.map((product) => (
-                <div key={product.id}>
-                    <img src={product.image}/>
-                    <h3>{product.name}</h3>
-                    <ul>
-                        <li>Калории: {product.kbzu.calories}</li>
-                        <li>Белки: {product.kbzu.protein}</li>
-                        <li>Жиры: {product.kbzu.fat}</li>
-                        <li>Углеводы: {product.kbzu.carbs}</li>
-                    </ul>
-                </div>
-            ))}
-        </Slider>
+        <div className="product-slider">
+            <h2>Продукты, полезные к употреблению:</h2>
+            <Slider {...settings} className="custom-slider">
+                {products.map((product) => (
+                    <div key={product.id}>
+                        <div className="product">
+                            <img src={product.image}/>
+                            <h3>{product.name}</h3>
+                            <ul>
+                                <li>Калории: {product.kbzu.calories}</li>
+                                <li>Белки: {product.kbzu.protein}</li>
+                                <li>Жиры: {product.kbzu.fat}</li>
+                                <li>Углеводы: {product.kbzu.carbs}</li>
+                            </ul>
+                        </div>
+                        
+                    </div>
+                ))}
+            </Slider>
+        </div>
     );
 };
 
